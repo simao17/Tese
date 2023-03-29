1157a7361c97c5f5eb8226b9ffdbc3a29d9a255c
simao17 <simiasmonteiro@gmail.com>
2023-03-29 02:19:08 +0100

    developments

--- translator.py
+++ translator.py
@@ -1,8 +1,11 @@
 from rdflib import Graph, Namespace
 from googletrans import Translator
-import deepl_api
+import deepl
 from deep_translator import LingueeTranslator
 import re
+
+auth_key = '8b2ab6f0-50c1-4212-8be6-ef0f49f583e4:fx'
+translator = deepl.Translator(auth_key)
 
 available_langs = {
     "mt": "maltese",
@@ -30,6 +33,10 @@ available_langs = {
     "lv": "latvian",
     "et": "estonian",
     "ja": "japanese"
+}
+
+deepl_langs = {
+    "pt": "pt-pt"
 }
 
 langs_pairs = [f"{key} - {value}" for key, value in available_langs.items()]
@@ -68,35 +75,70 @@ extracted = g.query(query, initNs={"rdf": RDF, "rdfs": RDFS, "owl": OWL})
 label_dict = {}
 for result in extracted:
     item_uri = result[0]
-    labellang = result[1].language
-    if str(labellang) == 'None':
-        labellang = lang
+    label_lang = result[1].language
+    if str(label_lang) == 'None':                        
+        label_lang = lang                               # Set label_lang to lang if it does'nt have any language associated
     if item_uri not in label_dict:
         label_dict[item_uri] = {}
-        label_dict[item_uri]['comments'] = set()            # Add a set to store comments
-    if labellang not in label_dict[item_uri]:
-        label_dict[item_uri][labellang] = []
-    if result[2] is not None:           # result[2] corresponds to the comment extracted
-        comment_lang = result[2].language or lang           # Set comment_lang to lang if it does'nt have any language associated
+        label_dict[item_uri]['comments'] = set()
+    if result[2] is not None:                           # result[2] corresponds to the comment extracted
+        comment_lang = result[2].language or lang       # Set comment_lang to lang if it does'nt have any language associated
         label_dict[item_uri]['comments'].add((result[2], comment_lang))         # Add comment to the set
-    if result[1] not in label_dict[item_uri][labellang]:
-        label_dict[item_uri][labellang].append(result[1])           # Append label to corresponding language
+    label_dict[item_uri][label_lang] = result[1]        # Assign label to corresponding language
 
-for item_uri, results in label_dict.items():
+"""for item_uri, results in label_dict.items():
     print(f"Item {item_uri}:")
     if 'comments' in results:
         for comment, comment_lang in results['comments']:
             print(f"  Comment ({comment_lang}): {comment}")
-    for labellang, label_list in results.items():
+    for labellang, label in results.items():
         if labellang != 'comments':
-            label_text = ', '.join(str(lit) for lit in label_list)
             print(f"  {labellang}:")
-            print(f"    {label_text}")
+            print(f"    {label}")"""
+
+translations_label_dict={}
+translations_comments_dict={}
+#Access every comment and every label to translate them
+for item_uri, item_data in label_dict.items():
+    translations_label_dict[item_uri]={}
+    translations_comments_dict[item_uri]={}
+
+    #Translate Comment
+    if item_data['comments']:
+        # Get first comment
+        comment, comment_lang = next(iter(item_data['comments']))   
+        #translate comment  
+        if(len(comment)!=0):
+            translated_comment = translator.translate_text(comment, source_lang=comment_lang, target_lang=deepl_langs[target_lang]).text
+        # Add translated comment to dictionary
+        translations_comments_dict[item_uri][target_lang] = translated_comment
+
+    #Translate label
+    for lang, label in item_data.items():
+        if lang != 'comments':          # Skip comments
+            #Translate label
+            if len(re.findall(r'\w+', label)) == 1:
+                #if the label is only one word, translate with Linguee because it gives several possible outputs and synonims
+                #try except block, because for some words p.e. "Mary", Linguee does not have translations
+                try:
+                    translated_label = LingueeTranslator(source=available_langs[lang], target=available_langs[target_lang]).translate(label, return_all=True)
+                except:
+                    translated_label = translator.translate_text(label, source_lang=lang, target_lang=deepl_langs[target_lang]).text
+            else:
+                #else, use deepl to translate, as it works better for sentences, and already should translate correctly depending on the context
+                translated_label = translator.translate_text(label, source_lang=lang, target_lang=deepl_langs[target_lang]).text
+            translations_label_dict[item_uri][target_lang] = translated_label
+            break
 
 
-#for translation in translations:
-#    print(translation.text)
+for item_uri, data in translations_label_dict.items():
+    print(f"Item {item_uri}:")
+    for label_lang, label in data.items():
+        print(f"  {label_lang}:")
+        print(f"    {label}")
 
-word = 'cuida-te'
-translated_word = LingueeTranslator(source='portuguese', target='english').translate(word, return_all=True)
-print(translated_word)
+for item_uri, data in translations_comments_dict.items():
+    print(f"Item {item_uri}:")
+    for comment_lang, comment in data.items():
+        print(f"  {comment_lang}:")
+        print(f"    {comment}")
