0cee71b5b092298f9afb007323ed1112658324b6
simao17 <simiasmonteiro@gmail.com>
2023-06-28 16:24:48 +0100

    translator2 looks promising

--- translator2.py
+++ translator2.py
@@ -1,8 +1,8 @@
 import re
 
-xml_file_path = "./teste2.xml"
+xml_file_path = "./teste4.xml"
 
-with open(xml_file_path, "r") as file:
+with open(xml_file_path, "r", encoding="utf-8") as file:
     lines = file.readlines()
 
 line_number = 0
@@ -12,41 +12,35 @@ for line in lines:
 
     if "<rdfs:label" in line:
         uri_start = line.find("<") + 1
-        uri_end = line.find(" ", uri_start)
-        if uri_end == -1 or line[uri_end - 1] == ">":
-            uri_end = line.find(">", uri_start)
-        uri = line[uri_start:uri_end]
+        uri_end = 10
+        uri = "rdfs:label"
 
         lang_match = re.search(r'xml:lang="([^"]*)"', line)
         lang = lang_match.group(1) if lang_match else None
 
         content_start = line.find(">", uri_end) + 1
         content_end = line.find("</rdfs:label>")
-        content = line[content_start:content_end].strip()
+        content = line[content_start:content_end].strip() or None
 
         print(f"Line {line_number}: URI: {uri}, Language: {lang}, Content: {content}")
 
     elif "<rdfs:comment" in line:
         uri_start = line.find("<") + 1
-        uri_end = line.find(" ", uri_start)
-        if uri_end == -1 or line[uri_end - 1] == ">":
-            uri_end = line.find(">", uri_start)
-        uri = line[uri_start:uri_end]
+        uri_end = 12
+        uri = "rdfs:comment"
 
         lang_match = re.search(r'xml:lang="([^"]*)"', line)
         lang = lang_match.group(1) if lang_match else None
 
         content_start = line.find(">", uri_end) + 1
         content_end = line.find("</rdfs:comment>")
-        content = line[content_start:content_end].strip()
+        content = line[content_start:content_end].strip() or None
 
         print(f"Line {line_number}: URI: {uri}, Language: {lang}, Content: {content}")
 
     elif "xml:lang=" in line:
         uri_start = line.find("<") + 1
         uri_end = line.find(" ", uri_start)
-        if uri_end == -1 or line[uri_end - 1] == ">":
-            uri_end = line.find(">", uri_start)
         uri = line[uri_start:uri_end]
 
         lang_match = re.search(r'xml:lang="([^"]*)"', line)
@@ -54,6 +48,6 @@ for line in lines:
 
         content_start = line.find(">", uri_end) + 1
         content_end = line.find("</")
-        content = line[content_start:content_end].strip()
+        content = line[content_start:content_end].strip() or None
 
         print(f"Line {line_number}: URI: {uri}, Language: {lang}, Content: {content}")
